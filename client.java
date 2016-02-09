9be7c1497a1c2c7145fe8ab97f6349dfd978fa22
Greg Friedman <friedmangreg37@ufl.edu>
2016-02-08 23:11:50 -0500

    Beginning of actual assignment: client connects to server and receives Hello! in response

--- client.java
+++ client.java
@@ -3,16 +3,25 @@ import java.net.*;
 
 class client {
 	public static void main(String argv[]) throws Exception {
-  		String sentence;
-  		String modifiedSentence;
-  		BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
-  		Socket clientSocket = new Socket("localhost", 4893);
-  		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-  		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-  		sentence = inFromUser.readLine();
-  		outToServer.writeBytes(sentence + '\n');
-  		modifiedSentence = inFromServer.readLine();
-  		System.out.println("FROM SERVER: " + modifiedSentence);
-  		clientSocket.close();
+		String input;
+		String response;
+		//input stream from user:
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		//create client socket and connect to server:
+		Socket clientSocket = new Socket("localhost", 4893);
+		//create output stream attacked to socket:
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		//create input stream attacked to socket:
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+		response = inFromServer.readLine(); 	//read line from server (should be "Hello!")
+		System.out.println("receive: " + response);		//print the message
+
+		//loop until user says to quit:
+		//while(true) {
+
+		//}
+
+		clientSocket.close();	//close the socket
  	}	
 }
--- server.java
+++ server.java
@@ -3,18 +3,26 @@ import java.net.*;
 
 class server {
 	public static void main(String argv[]) throws Exception {
-     	String clientSentence;
-        String capitalizedSentence;
+     	String clientMessage;
+        String response;
         ServerSocket welcomeSocket = new ServerSocket(4893);	//create socket at port 4893
 
         while(true) {
-         	Socket connectionSocket = welcomeSocket.accept();	//welcome socket waits for contact from client
+            //welcome socket waits for contact from client:
+         	Socket connectionSocket = welcomeSocket.accept();
+            //get IP address of connecting client:
+            String clientIP = connectionSocket.getInetAddress().getHostAddress();
+            //print message stating connection made:
+            System.out.println("get connection from " + clientIP);
+            //create input stream attached to socket:
             BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            //create output stream attached to socket:
             DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-            clientSentence = inFromClient.readLine();
-            System.out.println("Received: " + clientSentence);
-            capitalizedSentence = clientSentence.toUpperCase() + '\n';
-            outToClient.writeBytes(capitalizedSentence);
+
+            response = "Hello!\n";
+            outToClient.writeBytes(response);
+
+
         }
     }
 }
