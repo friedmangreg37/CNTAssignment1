335b00697efe34d4b9fa3a79ad562af5f04ce8bb
Greg Friedman <friedmangreg37@ufl.edu>
2016-02-09 13:39:20 -0500

    adding comments

--- client.java
+++ client.java
@@ -27,7 +27,9 @@ class client {
 			answer = Integer.parseInt(response);	//convert to an int
 			if(answer >= 0) {		//if >= 0 then not an error so just print result
 				System.out.println("receive: " + answer);
-			}else if(answer == -1) {
+			}
+			//error codes:
+			else if(answer == -1) {
 				System.out.println("receive: incorrect operation command");
 			}else if(answer == -2) {
 				System.out.println("receive: number of inputs is less than two");
@@ -36,13 +38,13 @@ class client {
 			}else if(answer == -4) {
 				System.out.println("receive: one or more of the inputs contain(s) non-number(s)");
 			}else if(answer == -5) {
-				//error code for exit, for print this, then close the socket:
+				//error code for exit, so print this, then close the socket:
 				System.out.println("receive: exit");
 				break;
 			}else {
 				System.out.println("receive: invalid error code");	//should never happen
 			}
 		}
-		clientSocket.close();
+		clientSocket.close();	//disconnect the client when we break from the loop
  	}	
 }
--- server.java
+++ server.java
@@ -27,49 +27,69 @@ class server {
             while(true) {
                 clientMessage = inFromClient.readLine();    //get the command from the client
                 System.out.print("get: " + clientMessage + ", return: ");
-                if(clientMessage.equals("bye")) {
+                if(clientMessage.equals("bye")) {   //client wants to exit
+                    //so set response accordingly, print it and send to client
                     response = "-5";
                     System.out.println(response);
                     outToClient.writeBytes(response + '\n');
-                    break;
+                    break;      //then exit loop since client will disconnect
                 }
+                //get an array of the words send from client:
                 String words[] = clientMessage.split(" ");
+                //add command:
                 if(words[0].equals("add")) {
                     try {
+                        //try converting each operand to int:
                         int answer = Integer.parseInt(words[1]);
                         for(int i = 2; i < words.length; i++) {
+                            //and add it to the running total:
                             answer += Integer.parseInt(words[i]);
                         }
+                        //convert answer back to string:
                         response = Integer.toString(answer);
                     }catch(NumberFormatException e) {
+                        //one or more operands wasn't a number, so send corresponding message:
                         response = "-4";
                     }
-                }else if(words[0].equals("subtract")) {
+                }
+                //subtract command:
+                else if(words[0].equals("subtract")) {
                     try {
+                        //try converting each operand to int:
                         int answer = Integer.parseInt(words[1]);
                         for(int i = 2; i < words.length; i++) {
+                            //and subtract it from the running total:
                             answer -= Integer.parseInt(words[i]);
                         }
+                        //convert answer back to string:
                         response = Integer.toString(answer);
                     }catch(NumberFormatException e) {
+                        //one or more operands wasn't a number, so send corresponding message:
                         response = "-4";
                     }
-                }else if(words[0].equals("multiply")) {
+                }
+                //multiply command:
+                else if(words[0].equals("multiply")) {
                     try {
+                        //try convert each operand to int:
                         int answer = Integer.parseInt(words[1]);
                         for(int i = 2; i < words.length; i++) {
+                            //and multiply to running total:
                             answer *= Integer.parseInt(words[i]);
                         }
+                        //convert answer back to string:
                         response = Integer.toString(answer);
                     }catch(NumberFormatException e) {
+                        //one or more operands wasn't a number, so send corresponding message:
                         response = "-4";
                     }
                 }
                 else {
+                    //if we're here, the first word wasn't a valid operation command
                    response = "-1";
                 }
-                System.out.println(response);
-                outToClient.writeBytes(response + '\n');
+                System.out.println(response);   //print the result to return
+                outToClient.writeBytes(response + '\n');    //and send it to the client
             }
         }
     }
