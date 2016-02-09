2a0a159b52ac245b3dd77dabdb75dfd193695883
Greg Friedman <friedmangreg37@ufl.edu>
2016-02-08 22:29:17 -0500

    Initial communication between client and server

--- server.java
+++ server.java
@@ -1,5 +1,20 @@
+import java.io.*;
+import java.net.*;
 
+class server {
+	public static void main(String argv[]) throws Exception {
+     	String clientSentence;
+        String capitalizedSentence;
+        ServerSocket welcomeSocket = new ServerSocket(4893);	//create socket at port 4893
 
-public class server {
-	
+        while(true) {
+         	Socket connectionSocket = welcomeSocket.accept();	//welcome socket waits for contact from client
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            clientSentence = inFromClient.readLine();
+            System.out.println("Received: " + clientSentence);
+            capitalizedSentence = clientSentence.toUpperCase() + '\n';
+            outToClient.writeBytes(capitalizedSentence);
+        }
+    }
 }
